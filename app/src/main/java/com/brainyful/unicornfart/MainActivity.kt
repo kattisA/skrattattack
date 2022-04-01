70cb92b39d5a3c3352ec636a4db87315f2a8a0f5
Katarina Lejonlid <katarina.lejonlid@qurit.se>
2022-04-01 19:19:39 +0200

    Unicorn icon with fart sound on click

--- app/src/main/java/com/brainyful/unicornfart/MainActivity.kt
+++ app/src/main/java/com/brainyful/unicornfart/MainActivity.kt
@@ -2,10 +2,37 @@ package com.brainyful.unicornfart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.media.MediaPlayer
+import android.view.View
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+    var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val unicornButton = findViewById<ImageButton>(R.id.playButton)
+        unicornButton.setOnClickListener { playSound(it)}
+    }
+
+    private fun playSound(view: View){
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(this,R.raw.fartsound2891)
+            mediaPlayer!!.isLooping = false
+            mediaPlayer!!.start()
+        } else if(mediaPlayer?.isPlaying == false)
+            mediaPlayer!!.isLooping = false
+            mediaPlayer!!.start()
+
+    }
+
+    // Destroys the MediaPlayer instance when the app is closed
+    override fun onStop() {
+        super.onStop()
+        if (mediaPlayer != null) {
+            mediaPlayer!!.release()
+            mediaPlayer = null
+        }
     }
 }
