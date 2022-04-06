cdae418e7e8f2526d14ef63b10a3fc863869e397
Katarina Lejonlid <katarina.lejonlid@qurit.se>
2022-04-06 15:02:03 +0200

    Random sounds and some sparkle

--- app/src/main/java/com/brainyful/unicornfart/MainActivity.kt
+++ app/src/main/java/com/brainyful/unicornfart/MainActivity.kt
@@ -8,20 +8,35 @@ import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     var mediaPlayer: MediaPlayer? = null
+    private val sounds: MutableList<MediaPlayer> = mutableListOf()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sound1 = MediaPlayer.create(this, R.raw.fartingdeflate3052)
+        val sound2 = MediaPlayer.create(this, R.raw.fartsound2891)
+        val sound3 = MediaPlayer.create(this, R.raw.sparklefart3052)
+        val sound4= MediaPlayer.create(this, R.raw.sparklefart3050)
+        sounds.add(sound1)
+        sounds.add(sound2)
+        sounds.add(sound3)
+        sounds.add(sound4)
+
         val unicornButton = findViewById<ImageButton>(R.id.playButton)
         unicornButton.setOnClickListener { playSound(it)}
+
+
     }
 
     private fun playSound(view: View){
         if (mediaPlayer == null) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.fartsound2891)
+            mediaPlayer = sounds.random()
             mediaPlayer!!.isLooping = false
             mediaPlayer!!.start()
         } else if(mediaPlayer?.isPlaying == false)
+            mediaPlayer= sounds.random()
             mediaPlayer!!.isLooping = false
             mediaPlayer!!.start()
     }
